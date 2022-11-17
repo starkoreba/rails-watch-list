0c2d5fb18f597ff29ef636ec45fc9b142b641bd9
Maeva De Campos <kulwilcoox@gmail.com>
2022-11-17 15:37:03 +0100

    initialisation controller + route + view

---
+++ app/controllers/lists_controller.rb
@@ -0,0 +1,33 @@
+class ListsController < ApplicationController
+  before_action :set_list, only: %i[show create]
+
+  def index
+    @lists = List.all
+  end
+
+  def show
+  end
+
+  def new
+    @list = List.new
+  end
+
+  def create
+    @list = List.new(params_list)
+    if @list.save
+      redirect_to list_path(@list)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def set_list
+    @list = List.find(params[:id])
+  end
+
+  def params_list
+    params.require(:list).permit(:name)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
+  get 'pages/home'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+  root to: 'pages#home', as: 'home'
+  get 'lists', to: 'lists#index'
+  get 'lists/:id', to: 'lists#show', as: 'list'
+  get 'lists/new', to: 'lists#new', as: 'new_list'
+  post 'lists/:id', to: 'list#create'
 end
